11dd6515e9c345b94a9ade56526b2abb6f56ac6a
Trina <catrutz@pdx.edu>
2019-03-09 17:34:11 -0800

    added uberjar profile to project

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot darkmoor.core
-  :repl-options {:init-ns darkmoor.core})
+  :repl-options {:init-ns darkmoor.core}
+  :profiles {:uberjar {:aot :all}})
